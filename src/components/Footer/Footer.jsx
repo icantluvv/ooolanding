8996fe3e8a7674ac80d58f4b08d0db6998934d41
Sergey <spanteleev670@gmail.com>
2023-06-25 16:59:37 +0300

    Fixing problems
    fixing problems with screen sizes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,53 +8,56 @@ import yt from './yt.png'
 
 const Footer = () => {
     return (
-        <div className={styles.footer}>
-            {/* <div>
-                
-            </div> */}
-            <div className={styles.container}>
-                <div className={styles.sources}>
-                    <div className={styles.lines}>
-                        <h4>Контакты</h4>
-                        <p>Почта: cleanrostovkupidon@mail.ru</p>
-                        <p>Телефон: +7(919)895-72-73</p>
-                    </div>
+        <>
+            <div className={styles.footer}>
+                {/* <div>
+                    
+                </div> */}
+                <div className={styles.container}>
+                    <div className={styles.sources}>
+                        <div className={styles.lines}>
+                            <h4>Контакты</h4>
+                            <p>Почта: cleanrostovkupidon@mail.ru</p>
+                            <p>Телефон: +7(919)895-72-73</p>
+                        </div>
 
-                    <div className={styles.lines}>
-                        <h4>О нас</h4>
-                        <a href="">
-                            <p>Документы</p>
-                        </a>
-                        <a href="">
-                            <p>Структура и органы управления организацией</p>
-                        </a>
-                        <a href="">
-                            <p>Основные сведения</p>
-                        </a>
-                    </div>
+                        <div className={styles.lines}>
+                            <h4>О нас</h4>
+                            <a href="">
+                                <p>Документы</p>
+                            </a>
+                            <a href="">
+                                <p>Структура и органы управления организацией</p>
+                            </a>
+                            <a href="">
+                                <p>Основные сведения</p>
+                            </a>
+                        </div>
 
-                    <div className={styles.lines}>
-                        <h4>Поддержка</h4>
-                        <a href="">
-                            <p>Частые вопросы клиентов</p>
-                        </a>
-                        <a href="">
-                            <p>Обратная связь</p>
-                        </a>
-                    </div>
+                        <div className={styles.lines}>
+                            <h4>Поддержка</h4>
+                            <a href="">
+                                <p>Частые вопросы клиентов</p>
+                            </a>
+                            <a href="">
+                                <p>Обратная связь</p>
+                            </a>
+                        </div>
 
-                    <div className={styles.lines}>
-                        <h4>Социальные сети</h4>
-                        <div className={styles.socialmedia}>
-                            <p><img src={vk} alt="" className={styles.vk}></img></p>
-                            <p><img src={yt} alt="" className={styles.yt}></img></p>
-                            <p><img src={tg} alt="" className={styles.tg}></img></p>
+                        <div className={styles.lines}>
+                            <h4>Социальные сети</h4>
+                            <div className={styles.socialmedia}>
+                                <p><img src={vk} alt="" className={styles.vk}></img></p>
+                                <p><img src={yt} alt="" className={styles.yt}></img></p>
+                                <p><img src={tg} alt="" className={styles.tg}></img></p>
+                            </div>
                         </div>
                     </div>
                 </div>
+                
             </div>
-            
-        </div>
+        </>
+
     )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import styles from './Header.module.css';
+// import logo from './logo.png';
 
 
 const Header = () => {
     return (
-        <div className={styles.header}>
-            <div className={styles.container}>
-                <div className={styles.sources}>
-                    <div className={styles.burger}>
-                        <a href='' className={styles.about}><h4>О НАС</h4></a>
-                        <a href='' className={styles.types}><h4>ВИДЫ РАБОТ</h4></a>
-                        <a href='' className={styles.contacts}><h4>КОНТАКТЫ</h4></a>
-                        <a href='' className={styles.os}><h4>ОТЗЫВЫ</h4></a>
+        <>
+            <div className={styles.header}>
+                <div className={styles.container}>
+                    <div className={styles.sources}>
+                        <div className={styles.logocont}>
+                            <a href="" className={styles.logo}><img src="./assets/images/logo.png"></img></a>
+                        </div>
+
+                        <div className={styles.hr}>
+                            <div className={styles.line}></div>
+                        </div>
+
+                        <div className={styles.burger}>
+                            <a href='' className={styles.about}><h4>О НАС</h4></a>
+                            <a href='' className={styles.types}><h4>ВИДЫ РАБОТ</h4></a>
+                            <a href='' className={styles.contacts}><h4>КОНТАКТЫ</h4></a>
+                            <a href='' className={styles.os}><h4>ОТЗЫВЫ</h4></a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 

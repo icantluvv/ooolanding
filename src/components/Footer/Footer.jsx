34a94790d32a198a9fa4c3d7ede60925dc972a8b
Sergey <spanteleev670@gmail.com>
2023-06-28 19:53:25 +0300

    Almost the end
    Made a full adaptive web-site. I need fix some mini-bugs and my work will be ended

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
                     
                 </div> */}
                 <div className={styles.container}>
-                    <div className={styles.sources}>
+                    <div id='voice' className={styles.sources}>
                         <div className={styles.lines}>
                             <h4>Контакты</h4>
                             <p>Почта: cleanrostovkupidon@mail.ru</p>
@@ -24,13 +24,13 @@ const Footer = () => {
                         <div className={styles.lines}>
                             <h4>О нас</h4>
                             <a href="">
-                                <p>Документы</p>
+                                <p className={styles.docs}>Документы</p>
                             </a>
-                            <a href="">
-                                <p>Структура и органы управления организацией</p>
+                            <a  href="">
+                                <p  className={styles.structure}>Структура и органы управления организацией</p>
                             </a>
-                            <a href="">
-                                <p>Основные сведения</p>
+                            <a  href="">
+                                <p  className={styles.osnova}>Основные сведения</p>
                             </a>
                         </div>
 
@@ -39,7 +39,7 @@ const Footer = () => {
                             <a href="">
                                 <p>Частые вопросы клиентов</p>
                             </a>
-                            <a href="">
+                            <a href="https://vk.com/topic-221175644_49161768">
                                 <p>Обратная связь</p>
                             </a>
                         </div>
@@ -47,9 +47,8 @@ const Footer = () => {
                         <div className={styles.lines}>
                             <h4>Социальные сети</h4>
                             <div className={styles.socialmedia}>
-                                <p><img src={vk} alt="" className={styles.vk}></img></p>
-                                <p><img src={yt} alt="" className={styles.yt}></img></p>
-                                <p><img src={tg} alt="" className={styles.tg}></img></p>
+                                <a href="https://vk.com/cleanrostovkupidon"><p><img src={vk} alt="" className={styles.vk}></img></p></a>
+                                <a href="https://t.me/vvhatineed"><p><img src={tg} alt="" className={styles.tg}></img></p></a>
                             </div>
                         </div>
                     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,10 +18,10 @@ const Header = () => {
                         </div>
 
                         <div className={styles.burger}>
-                            <a href='' className={styles.about}><h4>О НАС</h4></a>
-                            <a href='' className={styles.types}><h4>ВИДЫ РАБОТ</h4></a>
-                            <a href='' className={styles.contacts}><h4>КОНТАКТЫ</h4></a>
-                            <a href='' className={styles.os}><h4>ОТЗЫВЫ</h4></a>
+                            <a href='#about' className={styles.about}><h4>О НАС</h4></a>
+                            <a href='#type' className={styles.types}><h4>ВИДЫ РАБОТ</h4></a>
+                            <a href='#voice' className={styles.contacts}><h4>СВЯЗЬ</h4></a>
+                            <a href='https://vk.com/topic-221175644_49161768' className={styles.os}><h4>ОТЗЫВЫ</h4></a>
                         </div>
                     </div>
                 </div>
